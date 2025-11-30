07140425681ab1287d4470d9ab2022eed4fb965a
Fredrik Hegstam <fh@nuiteq.com>
2025-11-30 16:35:02 +0100

    Fix for memory leak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VideoProcessor } from '@pexip/media-processor';
 import { getVideoProcessor } from './video-processor';
+type SelectEffectType = 'none' | 'blur' | 'overlay';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,50 @@ import { getVideoProcessor } from './video-processor';
 export class AppComponent {
   title = 'media-processor-example';
   processedStream: MediaStream | null = null;
-  effect = 'none';
-  effects = ['none', 'blur', 'overlay'];
-
+  effect: SelectEffectType = 'none';
   private localStream: MediaStream | null = null;
   private currentVideoProcessor: VideoProcessor | null = null;
 
-  handleChangeEffect = async (event: any): Promise<void> => {
-    console.log('Changing effect', event.target.value);
+  private closeStreams() {
+    if(this.localStream) {
+      this.localStream.getTracks().forEach((track) => track.stop());
+    }
     this.currentVideoProcessor?.close();
+    this.processedStream = null;
+    this.localStream = null;
+  }
 
-    if(this.localStream !== null) {
-      this.currentVideoProcessor = await getVideoProcessor(event.target.value);
-      this.processedStream = await this.currentVideoProcessor.process(this.localStream);
+  async handleChangeEffect(event: Event): Promise<void> {
+    const value: SelectEffectType = (event.target as HTMLSelectElement).value as SelectEffectType;
+    console.log('Changing effect', value);
+    await this.currentVideoProcessor?.destroy();
+
+    try {
+      this.effect = value as SelectEffectType;
+      this.currentVideoProcessor = await getVideoProcessor(this.effect);
+      try {
+        if (this.currentVideoProcessor && this.localStream) {
+          this.processedStream = await this.currentVideoProcessor.process(this.localStream);
+        } else {
+          this.processedStream = this.localStream;
+        }
+      } catch (error) {
+        console.error('Error processing video stream:', error);
+        this.processedStream = null;
+      }
+    } catch (error) {
+      console.error('Error getting video processor:', error);
+    }
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadHandler(event: BeforeUnloadEvent) {
+    if(event) {
+      console.log('Unloadhandler triggered');
+      this.closeStreams();
+      this.effect = 'none';
+      const selectEffectElement = document.getElementById('selectEffect') as HTMLSelectElement;
+      selectEffectElement.value = this.effect;
     }
   }
 
@@ -45,8 +77,7 @@ export class AppComponent {
   }
 
   ngOnDestroy() {
-    if(this.localStream) {
-      this.localStream.getTracks().forEach((track) => track.stop());
-    }
+    this.closeStreams();
   }
+
 }
